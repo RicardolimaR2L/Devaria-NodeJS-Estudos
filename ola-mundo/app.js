6e1052eb6c54ea33f852de2d50ea96e4c928f3a3
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-04-18 20:35:39 -0300

    adicionar a descrição do exercício

--- ola-mundo/app.js
+++ ola-mundo/app.js
@@ -1,1 +1,13 @@
 console.log('Olá mundo, esse é meu primeiro programa Node.js')
+/*
+
+Olá mundo!
+
+Como nosso jargão, não poderia faltar o famoso Hello World nosso. Nesse exercício vamos exercitar algumas coisas:
+
+Criar um novo repositório;
+Criar nosso primeiro programa;
+Executá-lo na linha de comando;
+Obter a resposta na linha de comando.
+
+*/
